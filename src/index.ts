91e9a329c3854e87cfc71a9f9b17e04affa41bf4
lesenelir <hi@lesenelir.me>
2024-02-27 00:44:14 +0800

    feat: normal input to llm

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
+import OpenAI from 'openai'
 import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-console.log(process.env.OPENAI_API_KEY)
 
+async function main() {
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+  })
 
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: 'user', content: 'who are you ?' }],
+    model: 'gpt-3.5-turbo',
+  })
 
+  console.log(chatCompletion.choices[0].message.content)
+}
 
-
-
+main().then(() => {})
